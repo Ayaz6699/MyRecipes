24337afcf71b80c5f035abb9c78db66413759a79
Ayaz Nawaz <ayaz6699@gmail.com>
2016-08-08 19:17:24 +0400

    add pagination

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725173924) do
+ActiveRecord::Schema.define(version: 20160805101230) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20160725173924) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.boolean  "like"
+    t.integer  "chef_id"
+    t.integer  "recipe_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "recipes", force: :cascade do |t|
